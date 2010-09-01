96964c9f3a43f7c26e3788e0d25bb41e301dd06f
Lloyd Hilaiel <lloyd@mozilla.com>
2010-09-01 11:42:15 -0600

    update all production urls in test server, include scheme in substitution

--- harness/webserver.js
+++ harness/webserver.js
@@ -15,105 +15,108 @@
  */
 
 var sys = require("sys"),
-    http = require("http"),
-    url = require("url"),
-    path = require("path"),
-    fs = require("fs");
+http = require("http"),
+url = require("url"),
+path = require("path"),
+fs = require("fs");
 
 function createServer(port) {
-  return http.createServer(function(request, response) {
-    var hostname = request.headers['host'].toString("utf8");
+    return http.createServer(function(request, response) {
+        var hostname = request.headers['host'].toString("utf8");
 
-    var sites = {
-      "myapps.org": {
-        dir: "../site",
-        testhost: "localhost:8123"
-      },
-      "apptast.ic": {
-        dir: "../apptast.ic",
-        testhost: "localhost:8124"
-      },
-      "spaceface.com": {
-        dir: "../examples/spaceface.com",
-        testhost: "localhost:8125"
-      }
-    };      
+        var sites = {
+            "myapps.mozillalabs.com": {
+                dir: "../site",
+                prod_url: 'https://myapps.mozillalabs.com',
+                dev_host: "localhost:8123"
+            },
+            "apptastic.mozillalabs.com": {
+                dir: "../apptast.ic",
+                prod_url: 'https://apptastic.mozillalabs.com',
+                dev_host: "localhost:8124"
+            },
+            "spaceface.com": {
+                dir: "../examples/spaceface.com",
+                prod_url: 'http://spaceface.com',
+                dev_host: "localhost:8125"
+            }
+        };      
 
-    var siteroot = null;
-    for (s in sites) {
-      if (hostname == sites[s].testhost) {
-        console.log("req for host: " + s + " ("+ sites[s].testhost  + ")");
-        siteroot = sites[s].dir;
-        break;
-      }
-    }
-
-    if (siteroot === null) {
-      response.writeHead(404, {"Content-Type": "text/plain"});
-      response.write("404 Not Found");
-      response.end();
-      console.log("404 " + hostname);
-      return;
-    }
-
-    console.log("Siteroot: " + siteroot);
-
-    var uri = url.parse(request.url).pathname;
-    var filename = path.join(process.cwd(), siteroot, uri);
-
-    console.log("filename: " + siteroot);
-
-    var serveFile = function (filename) {
-      console.log("serving " + filename);
-      path.exists(filename, function(exists) {
-        if(!exists) {
-          response.writeHead(404, {"Content-Type": "text/plain"});
-          response.write("404 Not Found");
-          response.end();
-          sys.puts("404 " + filename);
-          return;
+        var siteroot = null;
+        for (s in sites) {
+            if (hostname == sites[s].dev_host) {
+                console.log("req for host: " + s + " ("+ sites[s].dev_host  + ")");
+                siteroot = sites[s].dir;
+                break;
+            }
         }
 
-        fs.readFile(filename, "binary", function(err, data) {
-          if(err) {
-            response.writeHead(500, {"Content-Type": "text/plain"});
-            response.write(err + "n");
+        if (siteroot === null) {
+            response.writeHead(404, {"Content-Type": "text/plain"});
+            response.write("404 Not Found");
             response.end();
-            sys.puts("500 " + filename);
+            console.log("404 " + hostname);
             return;
-          }
+        }
 
-          // if filename extension is .js, .css, or .html, let's search and replace
-          // all occurances of any of the hostnames with test hostnames
-          var textual = false;
-          var exts = [ ".js", ".css", ".html" ];
-          for (i in exts) {
-            if (true == (textual = (path.extname(filename) === exts[i]))) break;
-          }
+        console.log("Siteroot: " + siteroot);
 
-          if (textual && data && data.split) { 
-            for (s in sites) {
-              data = data.split(s).join(sites[s].testhost);
+        var uri = url.parse(request.url).pathname;
+        var filename = path.join(process.cwd(), siteroot, uri);
+
+        console.log("filename: " + siteroot);
+
+        var serveFile = function (filename) {
+            console.log("serving " + filename);
+            path.exists(filename, function(exists) {
+                if(!exists) {
+                    response.writeHead(404, {"Content-Type": "text/plain"});
+                    response.write("404 Not Found");
+                    response.end();
+                    sys.puts("404 " + filename);
+                    return;
+                }
+
+                fs.readFile(filename, "binary", function(err, data) {
+                    if(err) {
+                        response.writeHead(500, {"Content-Type": "text/plain"});
+                        response.write(err + "n");
+                        response.end();
+                        sys.puts("500 " + filename);
+                        return;
+                    }
+
+                    // if filename extension is .js, .css, or .html, let's search and replace
+                    // all occurances of any of the hostnames with test hostnames
+                    var textual = false;
+                    var exts = [ ".js", ".css", ".html" ];
+                    for (i in exts) {
+                        if (true == (textual = (path.extname(filename) === exts[i]))) break;
+                    }
+
+                    if (textual && data && data.split) { 
+                        for (s in sites) {
+                            data = data.split(sites[s].prod_url).join("http://" + sites[s].dev_host);
+                        }
+                    }
+
+                    response.writeHead(200);
+                    response.write(data, "binary");
+                    response.end();
+                    sys.puts("200 " + filename);
+                });
+            });
+        };
+
+        // automatically serve index.html if this is a directory
+        fs.stat(filename, function(err, s) {
+            if (err === null && s.isDirectory()) {
+                serveFile(path.join(filename, "index.html"));
+            } else {
+                serveFile(filename);
             }
-          }
-
-          response.writeHead(200);
-          response.write(data, "binary");
-          response.end();
-          sys.puts("200 " + filename);
         });
-      });
-    };
-
-    // automatically serve index.html if this is a directory
-    fs.stat(filename, function(err, s) {
-      if (err === null && s.isDirectory()) {
-        serveFile(path.join(filename, "index.html"));
-      } else {
-        serveFile(filename);
-      }
-    });
-  }).listen(port);
+    }).listen(port);
 };
 var ports = [8123, 8124, 8125];
 for (port in ports) createServer(ports[port]);
