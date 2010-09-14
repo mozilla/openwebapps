8e06727e4a601fe538b98c122a95f9c7e4429688
Lloyd Hilaiel <lloyd@hilaiel.com>
2010-09-14 14:24:54 -0700

    search full names in addition to screen names and tweets

--- examples/tweetsup/static/search.js
+++ examples/tweetsup/static/search.js
@@ -36,15 +36,16 @@
 	  cb(t);
 	}
       }
+      function searchTweet(t, term) {
+	var srchContent = (t.user.name + t.user.screen_name + t.text).toLowerCase();
+	return (srchContent.indexOf(term) >= 0);
+      }
 
       var url = 'query.php?token=' + sto.getItem('oauth_token') + "&secret=" + sto.getItem('oauth_secret')
 	+ "&path=statuses/friends_timeline.json&count=200&include_rts=true";
       var friends = $.getJSON(url, function(data) {
 	for (var i in data) {
-	  var srchContent = (data[i].user.screen_name + data[i].text).toLowerCase();
-	  if (srchContent.indexOf(term) >= 0) {
-	    returnTweet(data[i]);
-	  }
+	  if (searchTweet(data[i], term)) returnTweet(data[i]);
 	}
 	queryComplete(obj.id, friends);
       });
@@ -54,10 +55,7 @@
 	+ "&path=statuses/mentions.json&count=50";
       var mentions = $.getJSON(url, function(data) {
 	for (var i in data) {
-	  var srchContent = (data[i].user.screen_name + data[i].text).toLowerCase();
-	  if (srchContent.indexOf(term) >= 0) {
-	    returnTweet(data[i]);
-	  }
+	  if (searchTweet(data[i], term)) returnTweet(data[i]);
 	}
 	queryComplete(obj.id, mentions);
       });
