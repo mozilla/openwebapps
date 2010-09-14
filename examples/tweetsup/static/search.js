39fb98eb06bbe0978b97008a8bd05c861dc231ce
Lloyd Hilaiel <lloyd@hilaiel.com>
2010-09-14 15:41:40 -0700

    introduce fuzzy query caching to improve responsiveness and reduce the likelyhood of hitting per-user api limits

--- examples/tweetsup/static/search.js
+++ examples/tweetsup/static/search.js
@@ -3,6 +3,62 @@
   var searchId = 1000;
   var sto = window.localStorage;
 
+  var queryCache = {
+  };
+
+  /* run a query against twitter using a local cache
+   * type -- one of 'directs', 'mentions', or 'friends'
+   *
+   * cb -- a calback to invoke when results are available
+   * returns null or a reference to a jquery ajax object when
+   *   a call actually results in a http request.
+   */
+  function searchTwitter(type, cb) {
+    var typeToUrlPart = {
+      directs: {
+	urlpart: 'direct_messages',
+	extra: 'count=100'
+      },
+      mentions: {
+	urlpart: 'statuses/mentions',
+	extra: 'count=50'
+      },
+      friends: {
+	urlpart: 'statuses/friends_timeline',
+	extra: 'count=200&include_rts=true'
+      }
+    };
+    if (!typeToUrlPart[type]) throw("unknown query type: " + type);
+
+    var q = null;
+
+    // can haz cache?
+    if (queryCache[type] && (new Date() - queryCache[type].maxAge) <= queryCache[type].stamp) {
+//      console.log("returning data for '" + type + "' query from cache, it's only " + ((new Date() - queryCache[type].stamp)/1000.0).toFixed(2) + "s old" +
+//		 "(we'll let it live until its " + (queryCache[type].maxAge / 1000).toFixed(0) + "s)");
+      setTimeout(function() { cb(queryCache[type].data); }, 0);
+    } else {
+      delete queryCache[type];
+      if (!sto.getItem('oauth_token') || !sto.getItem('oauth_secret')) throw 'E_NEEDS_AUTH';
+
+      var url = 'query.php?token=' + sto.getItem('oauth_token') + "&secret=" + sto.getItem('oauth_secret')
+	+ "&path=" + typeToUrlPart[type].urlpart + '.json';
+      if (typeToUrlPart[type].extra) url += "&" + typeToUrlPart[type].extra
+
+      q = $.getJSON(url, function(data) {
+	// fuzzy cache.  let items live between 1 and two mins.  the fuzz decreases the likelyhood we'll run new queries
+        // all at once.
+	queryCache[type] = {
+	  maxAge: ((60 + Math.floor(Math.random()*61)) * 1000),
+	  data: data,
+	  stamp: new Date()
+	};
+	cb(data);
+      });
+    }
+    return q;
+  }
+
   function cancelSearch(id) {
     if (searches[id]) {
       // cancel outstanding queries
@@ -42,35 +98,14 @@
 	return (srchContent.indexOf(term) >= 0);
       }
 
-      var url = 'query.php?token=' + sto.getItem('oauth_token') + "&secret=" + sto.getItem('oauth_secret')
-	+ "&path=statuses/friends_timeline.json&count=200&include_rts=true";
-      var friends = $.getJSON(url, function(data) {
-	for (var i in data) {
-	  if (searchTweet(data[i], term)) returnTweet(data[i]);
-	}
-	queryComplete(obj.id, friends);
-      });
-      obj.queries.push(friends);
-
-      var url = 'query.php?token=' + sto.getItem('oauth_token') + "&secret=" + sto.getItem('oauth_secret')
-	+ "&path=statuses/mentions.json&count=50";
-      var mentions = $.getJSON(url, function(data) {
-	for (var i in data) {
-	  if (searchTweet(data[i], term)) returnTweet(data[i]);
-	}
-	queryComplete(obj.id, mentions);
-      });
-      obj.queries.push(mentions);
-
-      var url = 'query.php?token=' + sto.getItem('oauth_token') + "&secret=" + sto.getItem('oauth_secret')
-	+ "&path=direct_messages.json&count=100";
-      var directs = $.getJSON(url, function(data) {
-	for (var i in data) {
-	  if (searchTweet(data[i], term)) returnTweet(data[i]);
-	}
-	queryComplete(obj.id, directs);
-      });
-      obj.queries.push(directs);
+      var toSearch = [ 'directs', 'mentions', 'friends' ];
+      for (var i in toSearch) {
+	var searchTag = toSearch[i];
+	var q = searchTwitter(searchTag, function(data) {
+	  for (var j in data) if (searchTweet(data[j], term)) returnTweet(data[j]);
+	});
+	if (q) obj.queries.push(q);
+      }
 
       searches[obj.id] = obj;
       return obj.id;
