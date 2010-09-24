51584bae615f53a5f9e0698f13a892d0b2a01465
Lloyd Hilaiel <lloyd@mozilla.com>
2010-09-23 21:38:27 -0600

    complete basic port of dashboard to IE8

--- site/js/debug.js
+++
@@ -1,7 +0,0 @@
-// shim in a dump function if one doesn't exist
-if (!window.dump) {
-    if(typeof console === "undefined") {
-        window.console = { log: function() { } };
-    }
-    window.dump = function (x) { console.log(x); };
-}
