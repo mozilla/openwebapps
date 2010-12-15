52aae1734139596ab524e54e9d60379a7512016a
Lloyd Hilaiel <lloyd@hilaiel.com>
2010-12-15 00:33:22 -0800

    (chrome ext) when launching an application that's already running, find and focus the proper window+tab rather than opening fresh

--- addons/chrome/repo.js
+++ addons/chrome/repo.js
@@ -301,9 +301,27 @@
         if (!i || !i.app.base_url) return false;
         var baseURL = i.app.base_url;
         var launchURL = baseURL + (i.app.launch_path ? i.app.launch_path : "");
+        var appName = i.app.name;
+        var parsedBaseURL = URLParse(baseURL).normalize();
         
-        // no tab found with a containing url was found, let's, create one.
-        chrome.tabs.create({url: launchURL});
+        // determine if this application is running in some tab in some window
+        chrome.windows.getAll({populate:true}, function(windows) { 
+            for (var i = 0; i < windows.length; i++) {
+                var w = windows[i];
+                for (var j = 0; j < w.tabs.length; j++) {
+                    var t = w.tabs[j];
+                    if (parsedBaseURL.contains(t.url)) {
+                        console.log("found application running (" + appName + "), focusing");
+                        chrome.windows.update(w.id, { focused: true });                        
+                        chrome.tabs.update(t.id, { selected: true });
+                        return;
+                    }
+                }
+            }
+            console.log("app not running (" + appName + "), spawning");
+            chrome.tabs.create({url: launchURL});
+        });
+
         return true;
     }
 
