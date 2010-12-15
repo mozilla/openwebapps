209b9bab747aafee20eb91bc55bd71352b3bf42b
Lloyd Hilaiel <lloyd@hilaiel.com>
2010-12-15 00:04:19 -0800

    (chrome extension) api insertion is hard, lets go shopping:  for a chrome extension, we can reliably inject javascript code using the document_start content_script hook, but we cannot inject a script tag referencing a file -- under certain cached conditions injection code runs after other page javascript.  The fix is to insert a script tag containing inline injection code

--- addons/chrome/inject_api.js
+++ addons/chrome/inject_api.js
@@ -12,11 +12,102 @@
 
 // first let's inject our script to run inside the page's evaluation
 // context
+
+function inject_api() {
+    // a script that is inserted into the execution context of
+    // webpage.  his goal in life is to shim navigator.apps.
+    // to expose the OpenWebApps API and relay calls into trusted
+    // code.
+    if (!navigator.apps || navigator.apps.html5Implementation) {
+        var transactions = {};
+        var cur_trans_id = 1000;
+
+        var is_array = function(v) {
+            return v && typeof v === 'object' && v.constructor === Array;
+        }
+
+        // delay interaction with the dom (for the return listener) until
+        // a message is sent into the extension.  At this early stage of
+        // page load we cannot query the DOM.
+        var returnEventListenerRegistered = false;
+
+        var sendToExtension = function(action, args, cb) {
+            if (!returnEventListenerRegistered) {
+                returnEventListenerRegistered = true;
+                // now let's register for events incoming from the extension
+                document.getElementById("__openWebAppsOut").addEventListener('__openWebAppsOutEvent', function() {
+                    var data = document.getElementById('__openWebAppsOut').innerText;
+                    var msg = JSON.parse(data);
+                    if (transactions[msg.tid]) {
+                        var cb = transactions[msg.tid];
+                        delete transactions[msg.tid];
+                        cb(msg.resp);
+                    }
+                });
+            }
+
+            var aObj = { action: action };
+            if (args !== undefined) aObj.args = args;
+            if (cb) {
+                var i = cur_trans_id++;
+                transactions[i] = cb;
+                aObj.tid = i;
+            }
+            var div = document.getElementById("__openWebAppsIn");
+            div.innerText = JSON.stringify(aObj);
+            var ev = document.createEvent('Event');
+            ev.initEvent('__openWebAppsInEvent', true, true);
+            div.dispatchEvent(ev);
+        };
+
+        console.log("injecting navigator.apps API");
+        navigator.apps = {
+            getInstalled:function (cb) {
+                sendToExtension('getInstalled', undefined, cb);
+            },
+            getInstalledBy:function (cb) {
+                sendToExtension('getInstalledBy', undefined, cb);
+            },
+            install:function (obj) {
+                var cb = obj.callback;
+                delete obj.callback;
+                sendToExtension('install', obj, cb);
+            },
+            setRepoOrigin: function () {
+                console.log("WARNING: navigator.apps.setRepoOrigin is meaningless when the openwebapps extension is installed");
+            },
+            verify: function () {
+                console.log("verify called");
+            },
+            mgmt: {
+                launch:function (id) {
+                    sendToExtension('launch', id);
+                },
+                list: function (cb) {
+                    sendToExtension('list', undefined, cb);
+                },
+                remove: function (id) {
+                    sendToExtension('remove', { id: id }, (arguments.length == 2 ? arguments[1] : null));
+                },
+                loadState: function (cb) {
+                    sendToExtension('loadState', undefined, cb);
+                },
+                loginStatus: function (cb) {
+                    sendToExtension('loginStatus', undefined, cb);
+                },
+                saveState: function (obj, cb) {
+                    sendToExtension('saveState', obj, cb);
+                }
+            }
+        };
+    }
+}
+
 var owaContainer = document.createElement('div');
 owaContainer.style.display = "none";
 
 var s = document.createElement('script');
-s.src = chrome.extension.getURL("open_web_apps_api.js");
+s.innerHTML = "(" + inject_api.toString() +")();";
 owaContainer.appendChild(s);
 
 // now let's inject two custom DOM nodes that will be used for communication
