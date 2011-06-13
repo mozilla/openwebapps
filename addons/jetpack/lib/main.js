2ad4ea61db23b5c208d3713a0e65259ab7fdeff3
Ben Adida <ben@adida.net>
2011-06-13 09:47:44 -0700

    starting jetpack from existing restartless addon

---
+++ addons/jetpack/lib/main.js
@@ -0,0 +1,13 @@
+const widgets = require("widget");
+const tabs = require("tabs");
+
+var widget = widgets.Widget({
+  id: "mozilla-link",
+  label: "Mozilla website",
+  contentURL: "http://www.mozilla.org/favicon.ico",
+  onClick: function() {
+    tabs.open("http://www.mozilla.org/");
+  }
+});
+
+console.log("The add-on is running.");
