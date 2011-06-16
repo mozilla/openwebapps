52bdd7f1312f6b5eceffcfe23f99132aa46ea637
Anant Narayanan <anant@kix.in>
2011-06-16 08:57:01 -0700

    We replaced the popup with a dock

--- addons/jetpack/lib/ui.js
+++ addons/jetpack/lib/ui.js
@@ -110,9 +110,7 @@ openwebappsUI.prototype = {
         );
         doc.insertBefore(pi, doc.firstChild);
 
-        tmp = require("./panel");
         this._addToolbarButton();
-        this._popup = new tmp.appPopup(this._window);
         this._addDock();
     },
 
@@ -310,14 +308,6 @@ openwebappsUI.prototype = {
     _hideDock: function() {
         //this._dock.style.display ="none";
         this._dock.collapsed = true;
-    },
-    
-    _togglePopup: function() {
-        // Set up the current-app state:
-        this._repo.setCurrentPageAppURL(
-            this._window.gBrowser.contentDocument.applicationManifest
-        );
-        this._popup.toggle();
     }
 };
 
