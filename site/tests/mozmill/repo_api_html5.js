5b0ae64dbc46340d122d779427dd79dce82faeee
Shane Tomlinson <stomlinson@mozilla.com>
2011-06-27 14:43:56 -0700

    using XPath to get the anchor buttons instead of using a direct call to the link/port

--- site/tests/mozmill/repo_api_html5.js
+++ site/tests/mozmill/repo_api_html5.js
@@ -11,37 +11,58 @@ var setupTest = function(test) {
 
 var testFoo = function() {
   var timeout = 2000;
-  var button = new elementslib.ID(controller.tabs.activeTab.defaultView.frames[0].document, "installButton");
-  
-  for( var i = 0; i < 2; i++ ) {
-    controller.sleep(timeout);
-    controller.click(button);
-  }
+  var button = new elementslib.ID(
+          controller.tabs.activeTab.defaultView.frames[0].document, "installButton");
 
   controller.sleep(timeout);
   
-  var innerButton = new elementslib.ID(controller.tabs.activeTab.defaultView.frames[2].frames[0].document, "installButton");
-  controller.click( innerButton );
+  // Install the first two apps.  The first app installation causes the app 
+  // dashboard to appear, so we must set the tabIndex back to the original app.
+  for( var i = 0; i < 2; i++ ) {
+    controller.click(button);
+    controller.tabs.selectTabIndex(0);
+    controller.sleep(timeout);
+  }
+
+  // Sleep a few - now things get slightly ghetto.  The next app is installed 
+  // inside of another app.  Because of this, we have to get a reference to the 
+  // inner installButton and click it.
+  //controller.sleep(timeout);
   
+  var innerInstallButton = new elementslib.ID(
+          controller.tabs.activeTab.defaultView.frames[2].frames[0].document, "installButton");
+  controller.click( innerInstallButton );
+  
+  // The next two apps install as normal.
   for( var i = 0; i < 2; i++ ) {
     controller.sleep(timeout);
     controller.click(button);
   }
   
   controller.sleep(timeout);
-  var okButton = new elementslib.Link(controller.tabs.activeTab.defaultView.frames[0].document, "http://127.0.0.1:50007");
+
+  // Now, apps are running invokeService - these open up a dialog requesting 
+  // permission, we have to get references to the inner links to allow the 
+  // service to be used.
+  var okButton = new elementslib.XPath(
+          controller.tabs.activeTab.defaultView.frames[0].document, "/html/body/div[2]/ul/li/a");
   controller.click(okButton);
   
+  // A normal app install
   controller.sleep(2000);
   controller.click(button);
   
   controller.sleep(2000);
   
-  var okButtonImageSend = new elementslib.Link(controller.tabs.activeTab.defaultView.frames[0].document, "http://127.0.0.1:50006");
+  // Another invokeService being run, this time for image.send
+  var okButtonImageSend = new elementslib.XPath(
+          controller.tabs.activeTab.defaultView.frames[0].document, "/html/body/div[2]/ul/li/a");
   controller.click(okButtonImageSend);
   
+  // Wait for results stuff to finish.
   controller.sleep(5000);
 
-  var failCountElement = new elementslib.XPath(controller.tabs.activeTab, "/html/pre[@id='doctestOutput']/span[3]")
+  var failCountElement = new elementslib.XPath(
+          controller.tabs.activeTab, '//span[@class="failed"]')
   controller.assertText(failCountElement, '0');
 }
