358cb1ac4ae85af59bada0cc60617daf004bb3ed
Shane Tomlinson <stomlinson@mozilla.com>
2011-06-29 17:01:05 -0700

    Showing the same test in both JetPack and UnitTestRunner.
    This helps to demonstrate what is needed.

--- addons/jetpack/test/helpers/unit_test_runner.js
+++ addons/jetpack/test/helpers/unit_test_runner.js
@@ -1,70 +1,73 @@
-const keywords = [ 'setup', 'teardown', 'exports' ];
+const keywords = ['setup', 'teardown', 'exports'];
 
-function test_module( name, config ) {
+function test_module(name, config) {
+    var tests = {};
 
-    if( !config.exports ) {
-        throw 'add_test_module missing config value: exports';
-    }
-
-    for( var key in config ) {
-        if( keywords.indexOf( key ) === -1 ) {
-            config.exports[ name + '.' + key ] = wrapper.bind( null, config, config[ key ] );
+    for(var key in config) {
+        if(keywords.indexOf(key) === -1) {
+            config.exports[name + '.' + key] = wrapper.bind(null, config, config[key]);
         }
     }
+
+    return tests;
 }
 
-function wrapper( config, testFunc, testRunner ) {
-    if( config && config.setup ) {
+function wrapper(config, testFunc, testRunner) {
+    if(config && config.setup) {
         config.setup();
     }
 
-    // extend the testRunner with some utility functions
-    for( var key in testRunnerExtensions ) {
-        if( !testRunner[ key ] ) {
-            testRunner[ key ] = testRunnerExtensions[ key ];
+   // extend the testRunner with some utility functions
+    for(var key in testRunnerExtensions) {
+        if(!testRunner[key]) {
+            testRunner[key] = testRunnerExtensions[key];
         }
     }
 
-    testFunc.call( testRunner );
+    testFunc.call(testRunner);
 
-    if( config && config.teardown ) {
+    if(config && config.teardown) {
         config.teardown();
     }
 }
 
 
 const testRunnerExtensions = {
-    assertFunction: function( toTest, message ) {
-        return this.assertStrictEqual( 'function', typeof toTest, message );
+    assertFunction: function(toTest, message) {
+        return this.assertStrictEqual('function', typeof toTest, message);
     },
 
-    assertUndefined: function( toTest, message ) {
-        return this.assertStrictEqual( 'undefined', typeof toTest, message );
+    assertUndefined: function(toTest, message) {
+        return this.assertStrictEqual('undefined', typeof toTest, message);
     },
 
-    assertNotUndefined: function( toTest, message ) {
-        return this.assertNotStrictEqual( 'undefined', typeof toTest, message );
+    assertNotUndefined: function(toTest, message) {
+        return this.assertNotStrictEqual('undefined', typeof toTest, message);
     },
 
-    assertNull: function( toTest, message ) {
-        return this.assertStrictEqual( null, toTest, message );
+    assertNull: function(toTest, message) {
+        return this.assertStrictEqual(null, toTest, message);
     },
 
-    assertNotNull: function( toTest, message ) {
-        return this.assertNotStrictEqual( null, toTest, message );
+    assertNotNull: function(toTest, message) {
+        return this.assertNotStrictEqual(null, toTest, message);
     },
 
-    assertObject: function( toTest, message ) {
-        return this.assertStrictEqual( 'object', typeof toTest, message );
+    assertObject: function(toTest, message) {
+        return this.assertStrictEqual('[object Object]', Object.prototype.toString.apply(toTest), message);
     },
 
-    assertString: function( toTest, message ) {
-        return this.assertStrictEqual( '[object String]', Object.prototype.toString.apply( toTest ), message );
+    assertString: function(toTest, message) {
+        return this.assertStrictEqual('[object String]', Object.prototype.toString.apply(toTest), message);
     },
 
-    assertArray: function( toTest, message ) {
-        return this.assertStrictEqual( '[object Array]', Object.prototype.toString.apply( toTest ), message );
-    }                 
+    assertArray: function(toTest, message) {
+        return this.assertStrictEqual('[object Array]', Object.prototype.toString.apply(toTest), message);
+    },
+
+    assertNumber: function(toTest, message) {
+        return this.assertStrictEqual('[object Number]', Object.prototype.toString.apply(toTest), message);
+    }
 };
 
 
