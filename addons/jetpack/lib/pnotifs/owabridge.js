63539fd91c037695808fcb585bc3565915cb5f70
Paul Sawaya <me@paulsawaya.com>
2011-07-14 11:42:11 -0700

    OWA services now works with notifications

--- addons/jetpack/lib/pnotifs/owabridge.js
+++ addons/jetpack/lib/pnotifs/owabridge.js
@@ -1,5 +1,7 @@
 const url = require("url");
 
+var {FFRepoImplService} = require("api");
+
 const pushNotifications = require("pnotifs/main");
 
 // TODO: Find a way to generate random UUIDs that doesn't require chrome authority.
@@ -10,26 +12,47 @@ OWANotificationsBridge = function() {
     var notifClients = {};
     
     function requestPermissions(window, params, callbackFunc) {
-        console.log('in requestPermissions');
         var postOfficeHost = url.URL(window.location).host;
         var postOfficePort = 8000;
-        if (!notifClients[postOfficeHost]) {
+        
+        var notifClient = notifClients[postOfficeHost];
+        if (!notifClient) {
             // For now, we'll assume that every notifications host is running the POST office
             // on port 8000. We should (TODO) insert that into the params for requestPermissions though.
             
             // We'll also assume that the client agent is running at the same address as the POST office.
             
             var postOfficeURL = 'http://' + postOfficeHost + ':' + postOfficePort + '/';
-            notifClients[postOfficeHost] = new pushNotifications.Client(postOfficeURL, postOfficeURL);
+            notifClient = new pushNotifications.Client(postOfficeURL, postOfficeURL);
+            notifClients[postOfficeHost] = notifClient;
             
             // For now, a random UUID is the identifier and password.
-            notifClients[postOfficeHost].username = genRandomUUID();
-            notifClients[postOfficeHost].password = '';
-            console.log("new UUID username: " + notifClients[postOfficeHost].username);
+            notifClient.username = genRandomUUID();
+            notifClient.password = '';
+            console.log("new UUID username: " + notifClient.username);
             
-            notifClients[postOfficeHost].createUserQueue(function() {
-               notifClients[postOfficeHost].authSuccess(params, callbackFunc, postOfficeHost + ':' + postOfficePort); 
+            notifClient.createUserQueue(function() {
+                notifClient.authSuccess(params, callbackFunc, postOfficeHost + ':' + postOfficePort); 
             });
+            
+            function onNotification(notifBody) {
+                FFRepoImplService.getAppByUrl(window.location, function(app) {
+                    if (!app) return;
+                    
+                    if (app.services && app.services['pushnotifications.notification']) {
+                        var services = require("services");
+                        var serviceInterface = new services.serviceInvocationHandler(window);
+                        serviceInterface.invokeService(window.wrappedJSObject,
+                                                       'pushnotifications.notification', 'notification',
+                                                       {'notif' : notifBody},
+                                                       function(result) {});
+                    }
+                    else {
+                        notifClient.displayNotification(notifBody);
+                    }
+                });
+            }
+            notifClient.onNotification = onNotification;
         }
     }
     
