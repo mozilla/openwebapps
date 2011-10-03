5e077bcb2244b82bfe1167f62f5e33d00426c3fb
David Clarke <dclarke@mozilla.com>
2011-10-02 21:36:19 -0700

    modifications to run_jstests.sh to rely less on $WORKSPACE for changing directories.

--- site/tests/scripts/run_jstests.sh
+++ site/tests/scripts/run_jstests.sh
@@ -15,21 +15,21 @@ source $VENV/bin/activate
 
 # Create paths we want for addons
 
-echo "Installing Jstestnetlib Requirements" 
-pushd $WORKSPACE/openwebapps/site/tests/jstestnetlib && pip install -q -r requirements.txt
+echo "Installing Jstestnetlib Requirements"
+
+pushd ../jstestnetlib && pip install -q -r requirements.txt
 popd
 
 echo "Starting nodejs "
 
 
-cd $WORKSPACE/openwebapps/site/tests/ 
+pushd ../ 
 forever stop 0 
 sleep 4
 forever start run.js
+popd
 echo "Starting JS tests..." `date`
 
-
-cd $WORKSPACE/openwebapps/site/tests/scripts
 # Some of these env vars are set in the Jenkins build step.
 BROWSERS=firefox
 XARGS="-v --with-xunit --with-jstests --jstests-server $JSTESTS_SERVER --jstests-suite typed --jstests-token $JSTESTS_TOKEN --jstests-browsers $BROWSERS --debug nose.plugins.jstests"
@@ -37,4 +37,4 @@ echo '**************** /qunit/ ****************'
 python run_jstests.py --jstests-url http://$HOSTNAME:60172/tests/spec/typed-storage.html?runnerType=jstestnet --xunit-file=nosetests.xml $XARGS
 python run_jstests.py --jstests-url http://$HOSTNAME:60172/tests/spec/manifest.html?runnerType=jstestnet --xunit-file=nosetests.xml $XARGS
 python run_jstests.py --jstests-url http://$HOSTNAME:60172/tests/spec/conduits.html?runnerType=jstestnet --xunit-file=nosetests.xml $XARGS
-pushd $WORKSPACE/openwebapps/site/tests/ && forever stop 0
+forever stop 0
