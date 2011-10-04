f783641e25c8c5c8698df97054c91b07ccdf5d30
David Clarke <dclarke@mozilla.com>
2011-10-04 11:48:00 -0700

    changes required for continuous integration

---
+++ site/tests/scripts/run_jstests.sh
@@ -0,0 +1,43 @@
+# This script should be called from within Hudson
+
+VENV=$WORKSPACE/venv
+LOG=$WORKSPACE/jstests-runserver.log
+
+echo "Starting build on executor $EXECUTOR_NUMBER..." `date`
+
+
+if [ ! -d "$VENV/bin" ]; then
+echo "No virtualenv found. Making one..."
+  virtualenv $VENV
+fi
+
+source $VENV/bin/activate
+
+# Create paths we want for addons
+
+echo "Installing Jstestnetlib Requirements"
+
+pushd $WORKSPACE/site/tests/jstestnetlib && pip install -r requirements.txt && python setup.py install
+popd
+
+echo "Starting nodejs "
+
+
+pushd $WORKSPACE/site/tests 
+forever stop 0 
+sleep 4
+forever start run.js
+popd
+echo "Starting JS tests..." `date`
+
+cd $WORKSPACE/site/tests/scripts
+# Some of these env vars are set in the Jenkins build step.
+if [ -z "$BROWSERS" ]; then
+  BROWSERS="firefox"
+fi
+XARGS="-v --with-xunit --with-jstests --jstests-server $JSTESTS_SERVER --jstests-suite typed --jstests-token $JSTESTS_TOKEN --jstests-browsers $BROWSERS --debug nose.plugins.jstests"
+echo "******** $BROWSERS  ******** running the unit tests"
+python run_jstests.py --jstests-url http://$HOSTNAME:60172/tests/spec/typed-storage.html?runnerType=jstestnet --xunit-file=nosetests.xml $XARGS
+python run_jstests.py --jstests-url http://$HOSTNAME:60172/tests/spec/manifest.html?runnerType=jstestnet --xunit-file=nosetests.xml $XARGS
+python run_jstests.py --jstests-url http://$HOSTNAME:60172/tests/spec/conduits.html?runnerType=jstestnet --xunit-file=nosetests.xml $XARGS
+forever stop 0
