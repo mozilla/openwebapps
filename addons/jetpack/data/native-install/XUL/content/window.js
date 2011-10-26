981faabcd58c218367ce0ddef23756b4d102b509
Dan Walkowski <dwalkowski@mozilla.com>
2011-10-26 16:08:58 -0700

    apps now quit when all windows are closed. File menu allows opening multiple apps windows.

---
+++ addons/jetpack/data/native-install/XUL/content/window.js
@@ -0,0 +1,28 @@
+function doHandleMenuBar(toCall)
+{
+    // We need to pageMod in from the faker
+    // TODO pass this into content code somehow
+    window.alert("Menu bar item " + toCall + " was clicked!");
+    return;
+}
+
+window.addEventListener("click", function(e) {
+    // Make sure clicks remain in our context
+    // TODO check to see if we are in same origin?
+    if (e.target.nodeName == "A") {
+        e.preventDefault();
+        window.location = e.target.href;
+    }
+}, false);
+
+
+
+// Commands:
+var appName = "$APPNAME";
+function newWindow()
+{
+    var ww = Components.classes["@mozilla.org/embedcomp/window-watcher;1"]
+                       .getService(Components.interfaces.nsIWindowWatcher);
+    var win = ww.openWindow(null, "chrome://webapp/content/window.xul",
+                            null, "chrome,centerscreen", null);
+}
