82ee4c8744fce07d79bb7240d8fdbf2b541b15ed
Ian Bicking <ianb@colorstudy.com>
2011-11-07 18:15:03 -0600

    Added http_authorization and deleteCollection methods
    Added addApplication, listUninstalled, removeDeletion for sync integration.  Add last_modified to all apps on install
    
    Fix a variety of logic bugs, use TypedStorage

--- sync/sync-service.js
+++ sync/sync-service.js
@@ -3,26 +3,42 @@ var SyncService = function (args) {
     throw 'You forgot new';
   }
   this.pollTime = args.pollTime;
+  if (this.pollTime === undefined) {
+    this.pollTime = null;
+  }
   if (this.pollTime !== null && typeof this.pollTime !== 'number') {
     throw 'Invalid pollTime argument (should be null or a number): ' + this.pollTime;
   }
   this.server = args.server;
   this.repo = args.repo;
-  this.storage = args.storage || localStorage;
-  var value = this.storage.getItem('lastSyncTime');
-  if (value) {
-    this._lastSyncTime = parseFloat(value);
-  } else {
-    this._lastSyncTime = null;
-  }
-  value = this.storage.getItem('lastSyncPut');
-  if (value) {
-    this._lastSyncPut = parseFloat(value);
-  } else {
-    this._lastSyncPut = null;
-  }
+  this.storage = args.storage || new TypedStorage().open('sync');
+  this.storage.get('lastSyncTime', function (value) {
+    if (value) {
+      this._lastSyncTime = parseFloat(value);
+    } else {
+      this._lastSyncTime = null;
+    }
+  });
+  this.storage.get('lastSyncPut', function (value) {
+    if (value) {
+      this._lastSyncPut = parseFloat(value);
+    } else {
+      this._lastSyncPut = null;
+    }
+  });
+  this._appTracking = null;
+  this.storage.get('appTracking', function (value) {
+    if (value === null) {
+      value = {};
+    }
+    this._appTracking = value;
+  });
   // This will get set if the server tells us to back off on polling:
   this._backoffTime = null;
+  this.onlogin = null;
+  this.onlogout = null;
+  // FIXME: we need to catch any Retry-After values, and call this:
+  this.onretryafter = null;
 };
 
 SyncService.prototype.toString = function () {
@@ -30,19 +46,58 @@ SyncService.prototype.toString = function () {
 };
 
 SyncService.prototype.login = function (assertionData, callback) {
-  this.server.login(assertionData, callback);
+  var self = this;
+  this.server.login(assertionData, function (error, result) {
+    if (error) {
+      if (callback) {
+        callback(error);
+      }
+      return;
+    }
+    if (self.onlogin) {
+      self.onlogin(result);
+    }
+    if (callback) {
+      callback(error, result);
+    }
+  });
 };
 
-SyncService.prototype.loginStatus = function () {
-  return this.server.loginStatus();
+SyncService.prototype.loggedIn = function () {
+  return this.server.loggedIn();
+};
+
+SyncService.prototype.logout = function (callback) {
+  var self = this;
+  this.server.logout(function (error, result) {
+    if (error) {
+      if (callback) {
+        callback(error);
+      }
+      return;
+    }
+    self.invalidateLogin();
+    callback(error, result)
+  });
+};
+
+// FIXME: this also get called anytime the server doesn't like our login
+SyncService.prototype.invalidateLogin = function () {
+  if (this.onlogout) {
+    this.onlogout();
+  }
 };
 
 SyncService.prototype.lastSyncTime = function () {
-  return this._lastSyncTime;
+  if (this._lastSyncTime) {
+    return this._lastSyncTime;
+  } else {
+    return 0;
+  }
 };
 
 SyncService.prototype._setLastSyncTime = function (timestamp) {
-  if (! timestamp) {
+  if ((! timestamp) && timestamp !== 0) {
     // FIXME: not sure if it should ever be valid not to give a timestamp
     timestamp = new Date().getTime();
   }
@@ -50,11 +105,13 @@ SyncService.prototype._setLastSyncTime = function (timestamp) {
     throw 'Must _setLastSyncTime to number (not ' + timestamp + ')';
   }
   this._lastSyncTime = timestamp;
-  this.storage.setItem('lastSyncTime', this._lastSynctime);
+  // Note that we're just storing this for later, so we don't need to know
+  // when it gets really saved:
+  this.storage.put('lastSyncTime', this._lastSyncTime);
 };
 
 SyncService.prototype._setLastSyncPut = function (timestamp) {
-  if (! timestamp) {
+  if ((! timestamp) && timestamp !== 0) {
     // FIXME: not sure if it should ever be valid not to give a timestamp
     timestamp = new Date().getTime();
   }
@@ -62,21 +119,39 @@ SyncService.prototype._setLastSyncPut = function (timestamp) {
     throw 'Must _setLastSyncPut to number (not ' + timestamp + ')';
   }
   this._lastSyncPut = timestamp;
-  this.storage.setItem('lastSyncPut', this._lastSyncPut);
+  // Note that we're just storing this for later, so we don't need to know
+  // when it gets really saved:
+  this.storage.put('lastSyncPut', this._lastSyncPut);
 };
 
-SyncService.prototype.syncNow = function (callback) {
+SyncService.prototype.setAppTracking = function (value) {
+  this._appTracking = value;
+  this.storage.put('appTracking', value);
+};
+
+SyncService.prototype.syncNow = function (callback, forcePut) {
   var self = this;
-  console.warn('starting syncNow');
+  if (console.group) console.group('Starting syncNow');
   this._getUpdates(function (error) {
-    if (error) {
+    if (error && ((! forcePut) || (! error.collection_deleted))) {
+      log('getUpdates error/terminating', {error: error});
+      if (console.endGroup) console.endGroup();
+      else if (console.groupEnd) console.groupEnd();
       if (callback) {
         callback(error);
       }
       return;
     }
+    if (error && error.collection_deleted) {
+      log('Collection is deleted, but ignoring');
+      // However, since it's been deleted we need to forget when we accessed it
+      self._setLastSyncTime(0);
+      self._setLastSyncPut(0);
+    }
     self._putUpdates(function (error) {
-      console.warn('finished syncNow', error);
+      log('finished syncNow', {error: error});
+      if (console.endGroup) console.endGroup();
+      else if (console.groupdEnd) console.groupEnd();
       if (callback) {
         callback(error);
       }
@@ -84,27 +159,53 @@ SyncService.prototype.syncNow = function (callback) {
   });
 };
 
-SyncService.prototype._getUpdates = function (callback) {
+SyncService.prototype.deleteCollection = function (reason, callback) {
   var self = this;
-  console.log('getUpdates');
-  server.get(this.lastSyncTime(), function (error, results) {
-    // FIXME: check error
+  if (typeof reason == 'string') {
+    reason = {reason: reason};
+  }
+  if (! reason.client_id) {
+    reason.client_id = 'unknown';
+  }
+  // FIXME: add client_id to reason (when we have a client_id)
+  this.server.deleteCollection(reason, function (error, result) {
     if (error) {
       if (callback) {
         callback(error);
       }
       return;
     }
-    console.log('getUpdates response', results);
+    self._setLastSyncTime(0);
+    self._setLastSyncPut(0);
+    callback(error, result);
+  });
+};
+
+SyncService.prototype._getUpdates = function (callback) {
+  var self = this;
+  this.server.get(this.lastSyncTime(), function (error, results) {
+    // FIXME: check error
+    log('Ran GET', {since: self.lastSyncTime(), results: results, error: error});
+    if (error) {
+      if (callback) {
+        callback(error);
+      }
+      return;
+    }
     self._processUpdates(results.applications, function (error) {
-      console.log('processUpdates finished');
+      log('finished processUpdates', {error: error, until: results.until});
+      if (error) {
+        callback(error);
+        return;
+      }
       self._setLastSyncTime(results.until);
       if (results.incomplete) {
+        log('Refetching next batch');
         self._getUpdates(callback);
         return;
       }
       if (callback) {
-        callback(error);
+        callback();
       }
     });
   });
@@ -116,12 +217,20 @@ SyncService.prototype._processUpdates = function (apps, callback) {
   var deletionsToRemove = [];
   var appsToUpdate = [];
   var self = this;
+  if ((! apps) || (! apps.length)) {
+    log('No apps to process');
+    callback();
+    return;
+  }
+  log('processing updates', {apps: apps, repo: this.repo, uninstall: this.repo.listUninstalled});
   this.repo.listUninstalled(function (deleted) {
     var deletedByOrigin = {};
     for (var i=0; i<deleted.length; i++) {
       deletedByOrigin[deleted[i].origin] = deleted[i];
     }
-    this.repo.list(function (existing) {
+    self.repo.list(function (existing) {
+      existing = objectValues(existing);
+      log('got existing stuff', {existing: existing, deleted: deleted});
       var existingByOrigin = {};
       for (var i=0; i<existing.length; i++) {
         existingByOrigin[existing[i].origin] = existing[i];
@@ -158,9 +267,9 @@ SyncService.prototype._processUpdates = function (apps, callback) {
         if (finished && expected == 0) {
           callback();
         }
-        console.log('expectedCallback', expected, finished);
+        log('expectedCallback', {expected: expected, finished: finished});
       };
-      console.log('results', {dr: deletionsToRemove, a: appsToAdd, d: appsToDelete});
+      log('results', {deleteRemovals: deletionsToRemove, toAdd: appsToAdd, toDelete: appsToDelete});
       for (i=0; i<deletionsToRemove.length; i++) {
         var origin = deletionsToRemove[i];
         expected++;
@@ -168,6 +277,7 @@ SyncService.prototype._processUpdates = function (apps, callback) {
       }
       for (i=0; i<appsToAdd.length; i++) {
         var app = appsToAdd[i];
+        app.remotelyInstalled = true;
         expected++;
         self.repo.addApplication(app.origin, app, expectedCallback);
       }
@@ -180,48 +290,107 @@ SyncService.prototype._processUpdates = function (apps, callback) {
         // Apparently the operations weren't async, so they've already
         // all run... or there was nothing to run.
         callback();
+        return;
       }
       finished = true;
-    }, function (error) {callback(error || true);});
-  }, function (error) {callback(error || true);});
+    }, function (error) {callback(error || undefined);});
+  }, function (error) {callback(error || undefined);});
 };
 
 SyncService.prototype._putUpdates = function (callback) {
-  console.log('putUpdates');
-  var now = new Date().getTime();
   var lastUpdate = this._lastSyncPut;
   var self = this;
   this.repo.list(function (appList) {
+    if (self._appTracking === null) {
+      log('appTracking has not been fetched yet, cancelling put');
+      return;
+    }
+    var appTracking = self._appTracking;
+    appList = objectValues(appList);
+    log('putUpdates processing', {appList: appList, lastPut: self._lastSyncPut});
     var toUpdate = [];
     for (var i=0; i<appList.length; i++) {
       var app = appList[i];
       if (! app.last_modified) {
+        log('App missing last_modified', {app: app});
+        app.last_modified = new Date().getTime();
+        // We'll update the repo, but we don't need to wait for that to finish,
+        // as we've updated "our" copy of the app
+        self.repo.addApplication(app.origin, app);
         // FIXME: this should signal some error, but to whom?
         continue;
       }
       if (app.sync) {
         continue;
       }
-      if ((! self._lastSyncPut) || app.last_modified > self._lastSyncPut) {
+      if ((! self._lastSyncPut)
+          || (! appTracking[app.origin])
+          || (app.last_modified > appTracking[app.origin])) {
         toUpdate.push(app);
       }
     }
     if (! toUpdate.length) {
+      log('No updates to send');
       if (callback) {
         callback();
       }
       return;
     }
-    console.log('got updates to putUpdates', toUpdate);
-    server.put(toUpdate, function (error, result) {
+    log('putUpdates', {updates: toUpdate});
+    self.server.put(toUpdate, function (error, result) {
+      log('server put completed', {error: error, result: result});
       if (error) {
         if (callback) {
           callback(error);
         }
         return;
       }
-      self._setLastSyncPut(now);
+      var tracking = this.storage.get('appTracking', function (tracking) {
+        for (var i=0; i<updates.length; i++) {
+          tracking[updates[i].origin] = updates[i].last_modified;
+        }
+        this.storage.put('appTracking', tracking);
+      });
+      self._setLastSyncPut(result.received);
       callback();
     });
-  }, function (error) {callback(error || true);});
+  }, function (error) {callback(error);});
 };
+
+function log(msg) {
+  if (typeof console == 'undefined' || (! console.log)) {
+    return;
+  }
+  var args = [msg];
+  for (var i=1; i<arguments.length; i++) {
+    var a = arguments[i];
+    if (a === undefined || a === null || a === "") {
+      continue;
+    }
+    if (typeof a == "object") {
+      for (var j in a) {
+        if (a.hasOwnProperty(j) && a[j] !== undefined && a[j] !== null && a[j] !== "") {
+          args.push(j + ":");
+          args.push(a[j]);
+        }
+      }
+    } else {
+      args.push(a);
+    }
+  }
+  console.log.apply(console, args);
+}
+
+function objectValues(o) {
+  if (o.length) {
+    // It's already an array
+    return o;
+  }
+  var result = [];
+  for (var i in o) {
+    if (o.hasOwnProperty(i)) {
+      result.push(o[i]);
+    }
+  }
+  return result;
+}
