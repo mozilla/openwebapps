84510c713f221522e7c16dd89a3f4dee8ab68ef5
Anant Narayanan <anant@kix.in>
2011-11-10 16:22:21 -0800

    Implement navigator.mozApps.verifyReceipt

--- addons/jetpack/data/native-install/XUL/content/window.js
+++ addons/jetpack/data/native-install/XUL/content/window.js
@@ -34,6 +34,32 @@ try {
   var injector = {};
   Components.utils.import("chrome://webapp/content/injector.js", injector);
   
+  function getInstallRecord(cb) {
+    var appDirectory = Components.classes["@mozilla.org/file/directory_service;1"].  
+                        getService(Components.interfaces.nsIProperties).  
+                        get("CurProcD", Components.interfaces.nsIFile);  
+              
+    var aNsLocalFile = Components.classes['@mozilla.org/file/local;1'].createInstance(Components.interfaces.nsILocalFile);
+    aNsLocalFile.initWithFile(appDirectory);
+    aNsLocalFile.appendRelativePath("installrecord.json");
+
+    Components.utils.import("resource://gre/modules/NetUtil.jsm");  
+    Components.utils.import("resource://gre/modules/FileUtils.jsm");  
+            
+    dump(aNsLocalFile.path + "\n");
+    NetUtil.asyncFetch(aNsLocalFile, function(inputStream, status) {  
+        if (!Components.isSuccessCode(status)) {  
+            // Handle error!  
+            console.log("ERROR: " + status + " failed to read file: " + inFile);
+            return;  
+        }  
+        var data = NetUtil.readInputStreamToString(inputStream, inputStream.available());  
+        var parsed = JSON.parse(data);
+        inputStream.close();
+        cb(parsed);
+    });
+  }
+
   injector.InjectorInit(window); // we inject on the XUL window and observe content-document-global-created //   theBrowser.contentWindow.wrappedJSObject); // wrappedJSObject the right thing here?
   window.appinjector.register({
     apibase: "navigator.mozApps",
@@ -41,35 +67,119 @@ try {
     script: null,
     getapi: function(contentWindowRef) {
       return function(callback) {
-        
-        var appDirectory = Components.classes["@mozilla.org/file/directory_service;1"].  
-                                                getService(Components.interfaces.nsIProperties).  
-                                                    get("CurProcD", Components.interfaces.nsIFile);  
-              
-        var aNsLocalFile = Components.classes['@mozilla.org/file/local;1'].createInstance(Components.interfaces.nsILocalFile);
-        aNsLocalFile.initWithFile(appDirectory);
-        aNsLocalFile.appendRelativePath("installrecord.json");
-
-        Components.utils.import("resource://gre/modules/NetUtil.jsm");  
-        Components.utils.import("resource://gre/modules/FileUtils.jsm");  
-            
-        dump(aNsLocalFile.path + "\n");
-        NetUtil.asyncFetch(aNsLocalFile, function(inputStream, status) {  
-          if (!Components.isSuccessCode(status)) {  
-            // Handle error!  
-            console.log("ERROR: " + status + " failed to read file: " + inFile);
-            return;  
-          }  
-          var data = NetUtil.readInputStreamToString(inputStream, inputStream.available());  
-          var parsed = JSON.parse(data);
-          inputStream.close();
-          callback(parsed);
-        });  
+        getInstallRecord(callback);
       }
     }
   });
 
-  function checkNativeIdentityDaemon(callingLocation, callback, options, success, failure)
+  window.appinjector.register({
+    apibase: "navigator.mozApps",
+    name: "verifyReceipt",
+    script: null,
+    getapi: function(contentWindowRef) {
+        return function(options, cb, verifyOnly) {
+            getInstallRecord(function(record) {
+                if (!record) {
+                    cb({"error": "Invalid Receipt"});
+                    return;
+                }
+
+                function base64urldecode(arg) {
+                    var s = arg;
+                    s = s.replace(/-/g, '+'); // 62nd char of encoding
+                    s = s.replace(/_/g, '/'); // 63rd char of encoding
+                    switch (s.length % 4) // Pad with trailing '='s
+                    {
+                        case 0: break; // No pad chars in this case
+                        case 2: s += "=="; break; // Two pad chars
+                        case 3: s += "="; break; // One pad char
+                        default: throw new InputException("Illegal base64url string!");
+                    }
+                    return window.atob(s); // Standard base64 decoder
+                }
+
+                function parseReceipt(rcptData) {
+                    // rcptData is a JWT.  We should use a JWT library.
+                    var data = rcptData.split(".");
+                    if (data.length != 3)
+                        return null;
+
+                    // convert base64url to base64
+                    var payload = base64urldecode(data[1]);
+                    var parsed = JSON.parse(payload);
+
+                    return parsed;
+                }
+                
+                var receipt = parseReceipt(record.install_data.receipt);
+                if (!receipt) {
+                    cb({"error": "Invalid Receipt"});
+                    return;
+                }
+                
+                // Two status "flags", one for verify XHR other for BrowserID XHR
+                // These two XHRs run in parallel, and the last one to finish invokes cb()
+                var assertion;
+                var verifyStatus = false;
+                var assertStatus = false;
+
+                var verifyURL = receipt.verify;
+                var verifyReq = new XMLHttpRequest();  
+                verifyReq.open('GET', verifyURL, true);  
+                verifyReq.onreadystatechange = function (aEvt) {  
+                    if (verifyReq.readyState == 4) {
+                        // FIXME: 404? Yeah, because that's what we get now
+                        // Hook up to real verification when it's done on AMO
+                        // and change this to 200 !!!
+                        verifyStatus = true;
+                        if (verifyReq.status == 404) {
+                            if (verifyOnly && typeof verifyOnly == "function") {
+                                verifyOnly(receipt);
+                            }
+                            if (verifyStatus && assertStatus)
+                                cb({"success": {"receipt": receipt, "assertion": assertion}});
+                        } else {
+                            if (verifyStatus && assertStatus)
+                                cb({"error": "Invalid Receipt: " + req.responseText});
+                        }
+                    }
+                };
+                verifyReq.send(null);
+
+                // Start BrowserID verification
+                var options = {"silent": true, "requiredEmail": receipt.user.value};
+                checkNativeIdentityDaemon(contentWindowRef.location, options, function(ast) {
+                    assertion = ast;
+                    if (!assertion) {
+                        cb({"error": "Invalid Identity"});
+                        return;
+                    }
+
+                    var assertReq = new XMLHttpRequest();
+                    assertReq.open('POST', 'https://browserid.org/verify', true);
+                    assertReq.onreadystatechange = function(aEvt) {
+                        if (assertReq.readyState == 4) {
+                            assertStatus = true;
+                            if (assertReq.status == 200) {
+                                if (verifyStatus && assertStatus)
+                                    cb({"success": {"receipt": receipt, "assertion": assertion}});
+                            } else {
+                                if (verifyStatus && assertStatus)
+                                    cb({"error": "Invalid Identity: " + assertReq.responseText});
+                            }
+                        }
+                    };
+
+                    var body = "assertion=" + encodeURIComponent(assertion) + "&audience=" +
+                        contentWindowRef.location.protocol + "//" + contentWindowRef.location.host;
+                    assertReq.send(body);
+                });
+            });
+        }
+    }  
+  });
+
+  function checkNativeIdentityDaemon(callingLocation, options, success, failure)
   {
     dump("CheckNativeIdentityDaemon " + callingLocation + " " + options + "\n");
     // XXX what do we do if we are not passed a requiredEmail?
@@ -77,6 +187,7 @@ try {
     if (!options || !options.requiredEmail) failure();
 
     var port = 7350;
+    var output, input, scriptableStream;
     var sockTransportService = Components.classes["@mozilla.org/network/socket-transport-service;1"]
         .getService(Components.interfaces.nsISocketTransportService);
     
@@ -113,14 +224,12 @@ try {
         var transport = sockTransportService.createTransport(null, 0, "127.0.0.1", port, null);
         transport.setEventSink(eventSink, threadMgr.currentThread);
         try {
-            var output = transport.openOutputStream(transport.OPEN_BLOCKING, 0, 0);
+            output = transport.openOutputStream(transport.OPEN_BLOCKING, 0, 0);
             output.write(buf, buf.length);
-            var input = transport.openInputStream(transport.OPEN_BLOCKING, 0, 0);
-            var scriptableStream = Components.classes["@mozilla.org/scriptableinputstream;1"]  
+            input = transport.openInputStream(transport.OPEN_BLOCKING, 0, 0);
+            scriptableStream = Components.classes["@mozilla.org/scriptableinputstream;1"]  
                                  .createInstance(Components.interfaces.nsIScriptableInputStream);  
             scriptableStream.init(input);
-            alert("finished setup");
-            
         } catch (e) {
             alert(e);
             port++;
@@ -136,7 +245,7 @@ try {
     script: null,
     getapi: function(contentWindowRef) {
         return function(callback, options) { // XXX what is the options API going to be?
-            checkNativeIdentityDaemon(contentWindowRef.location, callback, options, function(assertion) {
+            checkNativeIdentityDaemon(contentWindowRef.location, options, function(assertion) {
                 // success: return to caller
                 callback(assertion);
             }, function() {
