04c147246c48877afe0d8f140d092641f0781f81
Dan Walkowski <dwalkowski@mozilla.com>
2012-01-06 12:10:08 -0800

    commented out performance-affecting preferences from the prefs.js file used by native app bundles

--- addons/jetpack/data/native-install/XUL/defaults/preferences/prefs.js
+++ addons/jetpack/data/native-install/XUL/defaults/preferences/prefs.js
@@ -2,6 +2,7 @@ pref("browser.chromeURL","chrome://webapp/content/window.xul");
 pref("browser.hiddenWindowChromeURL", "chrome://webapp/content/main.xul");
 pref("toolkit.defaultChromeURI", "chrome://webapp/content/window.xul");
 pref("toolkit.singletonWindowType", true);
+// possibly useful for debugging, but not a good idea to leave enabled all the time
 // pref("extensions.logging.enabled", "true");
 // pref("dom.report_all_js_exceptions", "true");
 // pref("browser.dom.window.dump.enabled", true);
